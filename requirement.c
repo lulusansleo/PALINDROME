c1d1dce21d9d99cce900c194c22b1fecf4ee2e8a
lulusansleo <luan.bourbier@epitech.eu>
2023-06-12 14:56:16 +0200

    [fix] requirement return values

--- requirement.c
+++ requirement.c
@@ -18,11 +18,13 @@ int my_squareroot_synthesis(int nb)
 {
     int i = 1;
 
-    if (nb <= 0)
+    if (i == 0)
         return 0;
+    if (nb < 0)
+        return -1;
     while (i * i != nb) {
         if ((i * i) > nb)
-            return 0;
+            return -1;
         i++;
     }
     return i;
